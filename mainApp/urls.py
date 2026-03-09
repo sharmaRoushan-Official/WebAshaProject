1d47f1be1cc71812eef01f3f72775b0bbb7107d7
Aditya Pimpale <pimpaleaditya2@gmail.com>
2026-03-09 16:31:44 +0530

    # aditya-tempate-inhertance UI

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -6,4 +6,12 @@ urlpatterns = [
     path("", homePage, name="home"),
     path("about/", aboutPage, name="about"),
     path("events/", eventPage, name="events"),
+    path("blogs/", blogPage, name="blogs"),
+    path("classes/", classPage, name="classes"),
+    path("training/", trainingPage, name="training"),
+    path("team/", teamPage, name="team"),
+    path("testimonial/", testimonialPage, name="testimonial"),
+    path("gallery/", galleryPage, name="gallery"),
+    path("contact/", contactPage, name="contact"),
+    path("404/", error404Page, name="404"),
 ]
--- mainApp/views.py
+++ mainApp/views.py
@@ -4,15 +4,55 @@ from django.shortcuts import render
 
 
 def homePage(request):
-   if request.method == "GET":
-      return render(request, "mainApp/home.html")
+    if request.method == "GET":
+        return render(request, "mainApp/home.html")
 
 
 def aboutPage(request):
-   if request.method == "GET":
-      return render(request, "mainApp/about.html")
+    if request.method == "GET":
+        return render(request, "mainApp/about.html")
 
 
 def eventPage(request):
-   if request.method == "GET":
-      return render(request, "mainApp/event.html")
+    if request.method == "GET":
+        return render(request, "mainApp/event.html")
+
+
+def blogPage(request):
+    if request.method == "GET":
+        return render(request, "mainApp/blogs.html")
+
+
+def classPage(request):
+    if request.method == "GET":
+        return render(request, "mainApp/classes.html")
+
+
+def trainingPage(request):
+    if request.method == "GET":
+        return render(request, "mainApp/training.html")
+
+
+def teamPage(request):
+    if request.method == "GET":
+        return render(request, "mainApp/team.html")
+
+
+def testimonialPage(request):
+    if request.method == "GET":
+        return render(request, "mainApp/testimonial.html")
+
+
+def galleryPage(request):
+    if request.method == "GET":
+        return render(request, "mainApp/gallery.html")
+
+
+def contactPage(request):
+    if request.method == "GET":
+        return render(request, "mainApp/contact.html")
+
+
+def error404Page(request):
+    if request.method == "GET":
+        return render(request, "mainApp/404.html")
