0c40d065e0879f325d03bf20f22e5047c72bf8b9
Aditya Pimpale <pimpaleaditya2@gmail.com>
2026-03-19 16:17:06 +0530

    feat(profile): add user profile model
    - Introduce a new `Profile` model to store extended user information.
    - Link `Profile` to Django's built-in `User` model using a OneToOneField.
    - Include fields for personal details, contact information, profile image,
      enrolled courses, and user roles.
    
    This enhancement provides a dedicated place to store additional user
    attributes beyond what the default `User` model offers, facilitating
    richer user profiles and future features like personalized dashboards
    and course management.

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -13,3 +14,19 @@ class Course(models.Model):
 
     def __str__(self):
         return self.title 
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(blank=True)
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    phone = models.CharField(max_length=15, blank=True)
+    address = models.TextField(blank=True)
+    enrolled_courses = models.ManyToManyField(Course, blank=True)
+    date_joined_institute = models.DateField(auto_now_add=True)
+    is_student = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    def __str__(self):
+        return f"{self.user.username} ({self.first_name} {self.last_name})"
